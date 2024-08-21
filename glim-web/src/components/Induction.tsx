2b66993c9395ae6c4b7ffdb8c2f04681f0d554f4
Akihisa Yagi <caphosra@gmail.com>
2024-08-21 23:49:06 +0900

    Create a simple template

---
+++ glim-web/src/components/Induction.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import '../styles/Induction.css';
+
+function Induction(props: { ind: string }) {
+    return (
+        <div className="induction">
+            {props.ind}
+        </div>
+    );
+}
+
+export default Induction;
---
+++ glim-web/src/components/Step.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { ArcherElement } from "react-archer";
+import '../styles/Step.css';
+
+interface IStepProps {
+    id: string;
+    children_id: string[];
+    premises: string[];
+    goal: string;
+}
+
+function Step(props: IStepProps) {
+    return (
+        <ArcherElement id={props.id} relations={props.children_id.map(child_id => {
+            return {
+                targetId: child_id,
+                targetAnchor: "top",
+                sourceAnchor: "bottom",
+                style: { strokeColor: "blue", strokeWidth: 1 }
+            }
+        })}>
+            <div className="step">
+                <p>Premises: {props.premises}</p>
+                <p>Goal: {props.goal}</p>
+            </div>
+        </ArcherElement>
+    );
+}
+
+export default Step;
